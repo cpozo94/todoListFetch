3436d30ca6ced947c6cfe734eac4d381618269a5
cpozo94 <cpg.948@gmail.com>
2023-03-05 12:18:31 +0000

    updating files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import propTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTasks, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -14,13 +14,13 @@ const [todoList, setTodoList] = useState([]);
 
 
 const Nuevo = (e) => {
+	
 	  setTodo(e.target.value)
 };
 
 
 
 const handleKey = (event) => {
-	console.log("handleKey", event.key);
     if (event.key === 'Enter') {
 		//copiamos el array anterior y añadimos un elemento nuevo, mirar el deatlle de añadir un array
       setTodoList([...todoList,todo]);
@@ -29,9 +29,8 @@ const handleKey = (event) => {
     }
   };
 
-  const deleteItem = () => {
 
-  }
+//   onClick={{}=>setTodoList(todoList.filter((t,currentIndex)=> index != currentIndex))}
 
 
 
@@ -41,9 +40,9 @@ const handleKey = (event) => {
 			<h1 className="title">Todo List</h1>
 			<input className="form-control" type="text" placeholder="Add task" onChange={Nuevo} onKeyPress={handleKey} value={todo}  />
 			<ul className="list-group">
-			{todoList.map((item) => {
-        return <li className="list-group-item"  >{item} <i> <FontAwesomeIcon icon={faTrash}/></i></li>;
-      })}
+			{todoList.map((item,i) => {
+				return <li className="list-group-item" key={i}  >{item}  <FontAwesomeIcon icon={faTrash} onClick={() => setTodoList(todoList.filter((t, currentIndex) => i !== currentIndex))}/></li>;
+      })} 
 			</ul>
 			<p>{todoList.length} items left</p>
 		</div>

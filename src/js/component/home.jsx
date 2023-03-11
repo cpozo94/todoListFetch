648da4027f529603f1f34f9af4ebb5cd7741ebc4
cpozo94 <cpg.948@gmail.com>
2023-03-11 17:36:58 +0000

    updating files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,10 +6,14 @@ import { faTasks, faTrash, faCheck } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
   const [todoList, setTodoList] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const getTodoList = async () => {
+    setLoading(false);
     const data = await getTask();
     setTodoList(data);
+     // actualizamos el estado de carga
+     setLoading(false);
   };
 
   useEffect(() => {
@@ -17,11 +21,13 @@ const Home = () => {
   }, []);
 
   const addTask = async () => {
+    setLoading(true);
     const newTaskObject = { label: newTodo, done: false };
     const newTodoList = [...todoList, newTaskObject];
     await editListTask(newTodoList);
     getTodoList();
-    setNewTodo("");
+    setLoading(true);
+    
   };
 
   const deleteTask = async (taskIndex) => {
@@ -37,31 +43,44 @@ const Home = () => {
   };
 
   return (
-    <div className="text-center">
-      <div>
-        <input
-          placeholder="new task"
-          onChange={(e) => setNewTodo(e.target.value)}
-          onKeyPress={handleKey}
-        ></input>
-        <button onClick={addTask}>save</button>
-      </div>
-    
-      {todoList.length ? (
-        todoList.map((todo, index) => (
-          <div key={index}>
-            <p>
-              <FontAwesomeIcon
-                icon={faTrash}
-                onClick={() => deleteTask(index)}
-              />
-              {todo.label}
-            </p>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <h1 className="title">Todo List</h1>
+
+          <div>
+            <input
+              className="form-control"
+              placeholder="new task"
+              onChange={(e) => setNewTodo(e.target.value)}
+              onKeyPress={handleKey}
+            ></input>
+            <button onClick={addTask}>save</button>
           </div>
-        ))
-      ) : (
-        <p>No tasks yet.</p>
-      )}
+
+          {loading ? ( // agregamos una condición para mostrar el spinner
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          ) : (
+            todoList.length ? (
+              todoList.map((todo, index) => (
+                <li className="list-group.item" key={index}>
+                  <p>
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      onClick={() => deleteTask(index)}
+                    />
+                    {todo.label}
+                  </p>
+                </li>
+              ))
+            ) : (
+              <p>No tasks yet.</p>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
